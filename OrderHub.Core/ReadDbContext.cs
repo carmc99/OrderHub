3850165e437199ad025fca35844692e9ef4229aa
carmc99 <carmc99@gmail.com>
2026-01-15 11:46:25 -0500

    Definir un DBContext de solo lectura para queries

---
+++ OrderHub.Core/ReadDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderHub.Core.Customer.Repositories.EF;
+using OrderHub.Core.Order.Repositories.EF;
+using OrderHub.Core.Order.Repositories.EF.Entities;
+using OrderHub.Customer.Repositories.EF.Entities;
+
+namespace OrderHub.Core.Repositories.EF
+{
+    public class ReadDbContext : DbContext
+    {
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+
+        public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CustomerMapper());
+            modelBuilder.ApplyConfiguration(new OrderMapper());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
