f4699f229b1dbc24943dc350c15d7ae1911cf4a3
carmc99 <carmc99@gmail.com>
2026-01-14 23:39:01 -0500

    Eliminar decorador en Id CustomerModel

--- OrderHub.Core/Customer/Models/CustomerModel.cs
+++ OrderHub.Core/Customer/Models/CustomerModel.cs
@@ -5,7 +5,6 @@ namespace OrderHub.Customer.Models
 {
     public class CustomerModel
     {
-        [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Email { get; set; }
