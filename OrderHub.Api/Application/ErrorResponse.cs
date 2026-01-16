d11258e98da4f11a3ad50d6f2a64267bd07cea44
carmc99 <carmc99@gmail.com>
2026-01-15 19:19:49 -0500

    Agregar middlware para estandarizar salida de excepciones

---
+++ OrderHub.Api/Application/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderHub.Api.Application
+{
+    //RFC 7807
+    public class ErrorResponse
+    {
+        public string? Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string? Detail { get; set; }
+        public string? Instance { get; set; }
+
+        public Dictionary<string, string[]>? Errors { get; set; }
+    }
+}
